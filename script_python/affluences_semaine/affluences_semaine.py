dc4623988072ab60b7fae1be308c5bc0c8d0259a
Hugo SEQUIER <hugoz@L450G8002.square-it.grp>
2023-02-14 17:45:57 +0100

    Script python qui calcule l'affluence des demandes de la semaine (v1 car pas de BD) Mais je pense qu'à terme ce script ne servira à rien et qu'il faudra plutot faire avec des triggers dans la BD. A chaque demande incrémenter/décrementer une un compteur pour chaque jour et heure

--- script_python/affluences_semaine/affluences_semaine.py
+++ script_python/affluences_semaine/affluences_semaine.py
@@ -26,6 +26,8 @@ frequenceSemaine = {
     'Samedi' : 0,
     'Dimanche' : 0
 }
+
+frequenceHeure = {}
 # %% Fonctions
 
 def convertStringtoDateTime(data):
@@ -42,31 +44,43 @@ def convertStringtoDateTime(data):
 def getDayOfWeekFromDateTime(date):
     return date.strftime("%A")
 
+def getHourFromDatetime(date):
+    return date.strftime("%H")
+
 def convertEngDaytoFrDay(day):
     return [FrenchDay[idx] for idx in range(len(FrenchDay)) if EngDay[idx] == day][0]
 
 def countAffluence(jour):
     frequenceSemaine[jour] += 1
+
+def countAffluenceHour(hour):
+    if hour in frequenceHeure :
+        frequenceHeure[hour] += 1
+    else : frequenceHeure[hour] = 1
     
 def traitement(data) :
     dateF = convertStringtoDateTime(data)
     dayWeek = getDayOfWeekFromDateTime(dateF)
+    hourDay = getHourFromDatetime(dateF)
     jourSemaine = convertEngDaytoFrDay(dayWeek)
+    
     countAffluence(jourSemaine)
+    countAffluenceHour(hourDay)
+
 # %% Main
 
 date0 = "2023-02-14 15:30:00"
-date1 = "2023-02-15 15:30:00"
-date2 = "2023-02-16 15:30:00"
-date3 = "2023-02-17 15:30:00"
-date4 = "2023-02-18 15:30:00"
-date5 = "2023-02-19 15:30:00"
-date6 = "2023-02-20 15:30:00"
-date7 = "2023-02-21 15:30:00"
-date8 = "2023-02-22 15:30:00"
-date9 = "2023-02-08 15:30:00"
-date10 = "2023-02-04 15:30:00"
-date11 = "2023-02-24 15:30:00"
+date1 = "2023-02-15 15:10:00"
+date2 = "2023-02-16 16:30:00"
+date3 = "2023-02-17 17:30:00"
+date4 = "2023-02-18 18:30:00"
+date5 = "2023-02-19 14:30:00"
+date6 = "2023-02-20 08:30:00"
+date7 = "2023-02-21 11:30:00"
+date8 = "2023-02-22 17:00:00"
+date9 = "2023-02-08 18:00:00"
+date10 = "2023-02-04 19:50:00"
+date11 = "2023-02-24 20:30:00"
 
 listeDate = [date0,date1,date2,date3,date4,date5,date6,date7,date8,date9,date10,date11]
 
@@ -74,3 +88,4 @@ for date in listeDate :
     traitement(date)
 
 print(frequenceSemaine)
+print(frequenceHeure)
